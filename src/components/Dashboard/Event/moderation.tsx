81722f9a6e272fa697fc89c22842f4d376001f37
Dery <deryaprianto4@gmail.com>
2024-12-22 04:23:46 +0700

    update dashboard

--- src/components/Dashboard/Event/moderation.tsx
+++ src/components/Dashboard/Event/moderation.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useState } from 'react';
+import Image from 'next/image';
 import {
   Button,
   Modal,
@@ -79,68 +80,131 @@ const ModerationDashboardTabComponent = () => {
       <Button className='!text-black max-w-[125px] rounded-lg !h-[35px] text-xs !font-semibold !text-white border-[#0BB90B] bg-[#0BB90B] mt-1.5'>
         Update
       </Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+      <Modal isOpen={isOpen} size="3xl" onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
             <>
               <ModalBody className='p-7'>
-                <div className="text-black mb-7">
+                
+                <div className="text-black smb-7">
                   <h3 className="text-sm font-semibold">
                     Welcome Screen Settings
                   </h3>
                   <span className="text-xs text-[#909090]">
                     Set up a welcome screen that appears once for first-time guests.
                   </span>
-                  <Tabs aria-label="Options" variant="underlined" className="tabs-event !ml-0 !mt-5">
-                    <Tab key="cover" title="Cover">
-                      <div className="text-black mb-8">
-                        <h3 className="text-sm">
-                          Title
-                        </h3>
-                        <span className="text-xs text-[#909090]">
-                          What is the event title?
-                        </span>
-                        <Input type="text" radius="sm" className="mt-3 w-96 border border-solid border-[#dddddd] rounded-md" />
-                      </div>
 
-                      <div className="text-black mb-8">
-                        <h3 className="text-sm">
-                          When does it happen?
-                        </h3>
-                        <span className="text-xs text-[#909090]">
-                          Set when your event is scheduled to start.
-                        </span>
-                        <DatePicker
-                          className="mt-3 max-w-[180px] border border-solid border-[#dddddd] rounded-md input-date"
-                          value={value}
-                          radius="sm"
-                          // onChange={setValue}
-                          onChange={(newValue) => setValue(newValue!)}
-                        />
-                      </div>
+                  <div className='flex'>
+                    <div className='w-3/5'>
+                      <Tabs aria-label="Options" variant="underlined" className="tabs-event !ml-0 !mt-5">
+                        <Tab key="cover" title="Cover">
+                          <div className="text-black mb-8">
+                            <h3 className="text-sm">
+                              Title
+                            </h3>
+                            <span className="text-xs text-[#909090] font-semibold">
+                              What is the event title?
+                            </span>
+                            <Input type="text" radius="sm" className="mt-3 w-96 border border-solid border-[#dddddd] rounded-md" />
+                          </div>
 
-                      <div className='mt-8'>
-                        <div className="text-black mb-2">
-                          <h3 className="text-sm font-semibold">
-                            Background
-                          </h3>
-                          <span className="text-xs text-[#909090] font-semibold">
-                            Set a background for the welcome screen
-                          </span>
+                          <div className="text-black mb-8">
+                            <h3 className="text-sm">
+                              When does it happen?
+                            </h3>
+                            <span className="text-xs text-[#909090] font-semibold">
+                              Set when your event is scheduled to start.
+                            </span>
+                            <DatePicker
+                              className="mt-3 max-w-[180px] border border-solid border-[#dddddd] rounded-md input-date"
+                              value={value}
+                              radius="sm"
+                              // onChange={setValue}
+                              onChange={(newValue) => setValue(newValue!)}
+                            />
+                          </div>
+
+                          <div className='mt-8'>
+                            <div className="text-black mb-2">
+                              <h3 className="text-sm font-semibold">
+                                Background
+                              </h3>
+                              <span className="text-xs text-[#909090] font-semibold">
+                                Set a background for the welcome screen
+                              </span>
+                            </div>
+                          </div>
+                        </Tab>
+                        <Tab key="form" title="Guest Form">
+                          <p className='text-[#909090] text-sm'>
+                            Create a form to collect guest information before they proceed to your album. Add new fields as needed, or rearrange existing ones by dragging.
+                          </p>
+                        </Tab>
+                      </Tabs>
+                    </div>
+                    <div className='w-2/5 relative'>
+                      <div className='absolutes w-full'>
+                        <div>
+                          <div className="bg-cover h-[500px] bg-no-repeat bg-[url('/bg-1.jpg')]"></div>
+                          <p className='text-center text-[#b4b2b2] text-xs mt-1.5'>
+                            this is preview
+                          </p>
                         </div>
                       </div>
-                    </Tab>
-                    <Tab key="form" title="Guest Form">
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    </Tab>
-                  </Tabs>
+                      
+                      <div className='relative z-10 px-4 text-white'>
+                        <div className='absolute bottom-5 w-11/12 mb-2'>
+                          <div className="flex items-center justify-between mb-2">
+                            <div className='rounded-lg overflow-hidden'>
+                              <Image 
+                                src="/thumbsimg.png"
+                                alt="Background"
+                                width={70}
+                                height={50}
+                                priority
+                              />
+                            </div>
+                            <div>
+                              <Button color="danger" variant="bordered" className='bg-[#0BB90B] rounded-lg max-w-[80px] !text-xs !h-[33px] text-white border-[#0BB90B]'>
+                                <span className='text-lg'>+</span>Upload
+                              </Button>
+                            </div>
+                          </div>
+                          <div className='mb-4 pb-4 border-b border-solid border-[#cecece]'>
+                            <h1 className='text-xl font-bold'>
+                              Daraa & Arifin Weddings
+                            </h1>
+                            <span className='text-sm'>
+                              13 Aug 2024
+                            </span>
+                          </div>
+                          <p className='text-center text-sm'>
+                            230 Photos & Videos
+                            <svg 
+                              className='mx-auto'
+                              xmlns="http://www.w3.org/2000/svg" 
+                              width="24" 
+                              height="24" 
+                              viewBox="0 0 24 24" 
+                              fill="none" 
+                              stroke="currentColor" 
+                              strokeWidth="2" 
+                              strokeLinecap="round" 
+                              strokeLinejoin="round">
+                              <polyline points="6 9 12 15 18 9"></polyline>
+                            </svg>
+                          </p>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
                 </div>
                 <div className='flex items-center gap-3'>
                   <Button className='!text-black max-w-[125px] rounded-lg !h-[35px] text-xs !font-semibold !text-white border-[#0BB90B] bg-[#0BB90B] mt-1.5'>
                     Save
                   </Button>
-                  <Button variant="bordered" className='!text-[#000000] max-w-[100px] rounded-lg !h-[37px] text-xs !font-semibold border-[#DDDDDD] mt-1.5' onPress={onClose}>
+                  <Button variant="bordered" className='!text-[#000000] max-w-[100px] rounded-lg !h-[35px] text-xs !font-semibold border-[#DDDDDD] mt-1.5' onPress={onClose}>
                     Cancel
                   </Button>
                 </div>
