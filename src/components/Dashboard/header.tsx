37d4bd0ba5ec5104e79ac08eed3193b847a81d79
Dery <deryaprianto4@gmail.com>
2024-12-25 11:44:47 +0700

    update dashboard

--- src/components/Dashboard/header.tsx
+++ src/components/Dashboard/header.tsx
@@ -15,7 +15,7 @@ const HeaderDashboardComponent = () => {
       <nav className="sticky top-0 z-50 flex items-center justify-between sm:px-5 py-2 bg-white border-b border-solid border-[#dddddd]">
         <div>
           <div className="flex items-center">
-            <div className='mr-10'>
+            <div className='sm:mr-28'>
               <Image 
                 src="/logo.png"
                 alt="Logo"
@@ -23,6 +23,80 @@ const HeaderDashboardComponent = () => {
                 fill
                 priority
               />
+            </div>
+            <div className='ml-3'>
+              <div className="flex items-center space-x-4">
+                <Dropdown placement="bottom-start">
+                  <DropdownTrigger>
+                    <User
+                      as="button"
+                      avatarProps={{
+                        isBordered: false,
+                        className: "rounded-md",
+                        src:"/thumb.png"
+                      }}
+                      className="text-black gap-4"
+                      name="Dara & Arifin Weddings"
+                    />
+                  </DropdownTrigger>
+                  <DropdownMenu aria-label="User Actions" variant="flat">
+                    <DropdownItem key="settings">
+                      <div className="flex items-center gap-4">
+                        <Image 
+                          src="/small-thumb.png"
+                          alt="Thumb"
+                          className="!relative !w-[32px] !rounded-md"
+                          fill
+                          priority
+                        />
+                        <div>
+                          <div className='text-black'>
+                            <h2 className='text-xs'>
+                              Birthday Party
+                            </h2>
+                          </div>
+                        </div>
+                      </div>
+                    </DropdownItem>
+                    <DropdownItem key="logout">
+                      <div className="flex items-center gap-4">
+                        <Image 
+                          src="/thumb.png"
+                          alt="Thumb"
+                          className="!relative !w-[32px] !rounded-md"
+                          fill
+                          priority
+                        />
+                        <div>
+                          <div className='text-black'>
+                            <h2 className='text-xs'>
+                              Dara & Arifin Weddings
+                            </h2>
+                          </div>
+                        </div>
+                      </div>
+                    </DropdownItem>
+                    <DropdownItem key="store">
+                      <div className="flex items-center gap-4">
+                        <Image 
+                          src="/small-thumb.png"
+                          alt="Thumb"
+                          className="!relative !w-[32px] !rounded-md"
+                          fill
+                          priority
+                        />
+                        <div>
+                          <div className='text-black'>
+                            <h2 className='text-xs'>
+                              Store Opening
+                            </h2>
+                          </div>
+                        </div>
+                      </div>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </div>
             </div>
             {/* <div>
               <div className="hidden md:flex space-x-8">
@@ -61,7 +135,7 @@ const HeaderDashboardComponent = () => {
                 <User
                   as="button"
                   avatarProps={{
-                    isBordered: true,
+                    isBordered: false,
                     src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
                   }}
                   className="transition-transform text-black gap-4"
