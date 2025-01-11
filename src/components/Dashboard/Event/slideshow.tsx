d84469b3d6cb78a9b8be54b59d43281dd1540df2
Dery <deryaprianto4@gmail.com>
2025-01-12 00:44:01 +0700

    s

--- src/components/Dashboard/Event/slideshow.tsx
+++ src/components/Dashboard/Event/slideshow.tsx
@@ -1,11 +1,41 @@
 'use client';
+import React, { useContext, useState, useEffect } from 'react';
 import {
   Button,
   Switch,
   Input
 } from "@nextui-org/react";
+import { AppContext } from "../../../../contexts/ContextProviders";
 
 const SlideshowDashboardTabComponent = () => {
+  const { state } = useContext(AppContext);
+
+  const [formData, setFormData] = useState({
+    durationImage: '',
+    durationVideo: '',
+    hideSlideshowQR: state.showDetailEvent.slideShow?.hideSlideshowQR,
+    hideVideoSound: state.showDetailEvent.slideShow?.hideVideoSound
+  });
+
+  useEffect(() => {
+    if (state.showDetailEvent) {
+      setFormData((prev) => ({
+        ...prev,
+        durationImage: state.showDetailEvent.slideShow?.durationImage,
+        durationVideo: state.showDetailEvent.slideShow?.durationVideo,
+        hideSlideshowQR: state.showDetailEvent.slideShow?.hideSlideshowQR,
+        hideVideoSound: state.showDetailEvent.slideShow?.hideVideoSound,
+      }));
+    }
+  }, [state.showDetailEvent]);
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   return (
     <>
       <div className="text-black mb-2">
@@ -33,6 +63,9 @@ const SlideshowDashboardTabComponent = () => {
             <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
               <Input
                 key="image"
+                name="durationImage"
+                value={formData.durationImage}
+                onChange={handleChange}
                 radius="sm"
                 className="box-duration"
                 description="seconds"
@@ -47,6 +80,9 @@ const SlideshowDashboardTabComponent = () => {
             <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
               <Input
                 key="video"
+                name="durationVideo"
+                value={formData.durationVideo}
+                onChange={handleChange}
                 radius="sm"
                 className="box-duration"
                 description="seconds"
@@ -70,7 +106,7 @@ const SlideshowDashboardTabComponent = () => {
               Turn on to remove the QR code from the Slideshow page.
             </span>
           </div>
-          <Switch size="sm" />
+          <Switch name="hideSlideshowQR" defaultSelected={formData.hideSlideshowQR} onChange={handleChange} size="sm" />
         </div>
       </div>
       <div className='mt-8 mb-3'>
@@ -83,7 +119,7 @@ const SlideshowDashboardTabComponent = () => {
               This will enable videos sound when played in Slideshow.
             </span>
           </div>
-          <Switch size="sm" />
+          <Switch name="hideVideoSound" defaultSelected={formData.hideVideoSound} onChange={handleChange} size="sm" />
         </div>
       </div>
       <Button className='!text-black max-w-[125px] rounded-lg !h-[35px] text-xs !font-semibold !text-white border-[#0BB90B] bg-[#0BB90B] mt-1.5'>
